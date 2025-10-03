fbf5858d7528828d183a4e77c1f942d267265f18
blesed9 <blesed.wk@gmail.com>
2025-10-03 19:13:16 +0200

    add something

--- kiosk_configure.py
+++ kiosk_configure.py
@@ -14,7 +14,9 @@ def update_system():
 time.sleep(2)
 os.system("apt-get update")
 os.system("apt-get upgrade -y")
+os.system("sudo apt install xorg openbox chromium-browser unclutter lightdm -y")
 
+time.sleep(2)
 
 
 
