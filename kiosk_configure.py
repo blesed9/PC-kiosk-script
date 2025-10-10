55c0bc45fefe980177ae0eb6201753abfd788c68
blesed9 <blesed.wk@gmail.com>
2025-10-10 19:38:39 +0200

    add something

--- kiosk_configure.py
+++ kiosk_configure.py
@@ -7,7 +7,7 @@ if os.geteuid() != 0:
     "again, this time using 'sudo'. Exiting.")
 else: pass
 time.sleep(2)
-url = input("Podaj adres strony dla kiosku (np. https://example.com): ").strip()
+#url = input("Podaj adres strony dla kiosku (np. https://example.com): ").strip()
 
 def update_system():
     print("Update system...")
@@ -33,19 +33,22 @@ def create_user():
 os.system("adduser kiosk")
 os.system("usermod -aG audio,video kios")
 
-time.sleep()
+time.sleep(2)
 def create_files():
     print("Creating a files")
 os.system("nano /etc/lightdm/lightdm.conf")
-
-
-os.system("-u kiosk mkdir -p /home/kiosk/.config/openbox")
-os.system("-u kiosk nano /home/kiosk/.config/openbox/autostart")
-os.system('echo -e "[Seat:*]\\nautologin-user=kiosk\\nautologin-user-timeout=0" | sudo tee -a /etc/lightdm/lightdm.conf')
-
 time.sleep(2)
-
-
-
-
+os.system("-u kiosk mkdir -p /home/kiosk/.config/openbox")
+time.sleep(1)
+os.system("-u kiosk nano /home/kiosk/.config/openbox/autostart")
+time.sleep(1)
+os.system('echo -e "[Seat:*]\\nautologin-user=kiosk\\nautologin-user-timeout=0" | sudo tee -a /etc/lightdm/lightdm.conf')
+time.sleep(2)
+os.system('echo "#!/bin/bash && xmodmap -e \\"keycode 133 = \\" && xmodmap -e \\"keycode 64 = \\" && xmodmap -e \\"keycode 37 = \\" && xmodmap -e \\"keycode 135 = \\" && xmodmap -e \\"keycode 107 = \\" && xset s off && xset -dpms && xset s noblank && unclutter -idle 3 -root & KIOSK_URL=\\"https://strona-egzaminu.pl\\" && chromium --kiosk \$KIOSK_URL --no-first-run --disable --disable-translate --disable-infobars --disable-suggestions-service --disable-save-password-bubble --disable-session-crashed-bubble --disable-pinch --incognito --password-store=basic & while true; do sleep 15; if ! pgrep chromium > /dev/null; then chromium --kiosk \$KIOSK_URL --no-first-run --disable --disable-translate --disable-infobars --disable-suggestions-service --disable-save-password-bubble --disable-session-crashed-bubble --disable-pinch --incognito --password-store=basic & fi; done" > kiosk.sh')
+time.sleep(1)
+os.system("chmod +x /home/kiosk/.config/openbox/autostart")
+time.sleep(1)
+os.system("chown -R kiosk:kiosk /home/kiosk/.config")
+time.sleep(1)
+os.system("reboot")
 
